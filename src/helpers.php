e1cbb853458e831f4154cf73d43016c333bbebee
ahimu <575074803@qq.com>
2024-03-18 16:42:10 +0800

    1

--- src/helpers.php
+++ src/helpers.php
@@ -8,7 +8,7 @@ if (!function_exists('captcha')) {
      * @return array|ImageManager|mixed
      * @throws Exception
      */
-    function captcha(string $config = 'default')
+    function captcha(string $config = 'default'): mixed
     {
         return app('captcha')->create($config);
     }
